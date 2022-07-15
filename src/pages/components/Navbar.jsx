571e69d19602223b12df8a6ce1a463ef89093a82
Christopher Rattanasamay <c.rattanasamay@gmail.com>
2022-07-15 16:11:18 -0500

    Manifest.json

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -3,63 +3,40 @@ import styled from 'styled-components';
 
 
 const Container = styled.div`
-    margin:0px;
-    height: 6rem;
-    background-color: #5360ff;
-`
-const Wrapper = styled.div`
-    align-items: center;
-    padding: 1rem 2rem;
     display: flex;
     justify-content: space-between;
+    background-color: #5360ff;
+    align-items: center;
 `
 
-const Left = styled.div`
-    flex: 1;
-    text-align: left;
+const NavContainer = styled.div`
+   
 `
-const Center = styled.div`
-    flex: 1;    
-    text-align: center;
-`
-const Right = styled.div`
-    text-align: right;
-    display: flex;
-    flex: 1;
-    justify-content: flex-end; // Items Float To The End
-`
-const NavItem = styled.div`
+const NavItem = styled.li`
     font-size: 1.6rem;
-    padding-left: 2.5rem;
 `
 
 const LogoName = styled.h1`
     font-weight: bold;
+    float: left;
+`
+
+const NavList = styled.ul`
+
 `
 
 
 export default function Navbar() {
   return (
     <Container>
-        <Wrapper>
-            <Left>
-                <LogoName>
-                   Anime Stuff
-                </LogoName>
-               
-            </Left>
-            <Center>
-                <LogoName>
-                    WoW Center
-                </LogoName>
-            </Center>
-            <Right>
-                <NavItem>Home</NavItem>
-                <NavItem>Login</NavItem>
-                <NavItem>Search</NavItem>
-            </Right>
-
-        </Wrapper>
+        <LogoName>Anime Club</LogoName>
+            <NavContainer>
+                <NavList>
+                    <NavItem>Home</NavItem>
+                    <NavItem>Login</NavItem>
+                    <NavItem>Search</NavItem>
+                </NavList>
+           </NavContainer>
     </Container>
   )
 }
