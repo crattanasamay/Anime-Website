1c4842c72cbd481c78bc036e1a9dc5b6bf8dcb37
Christopher Rattanasamay <c.rattanasamay@gmail.com>
2022-07-27 17:45:41 -0500

    Revert "MUI Added and height fix"
    This reverts commit ace25df768ecc3b01a9e3b17c40f33c7f2868b2b.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,58 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
-import {TextField,Button,Box} from "@mui/material";
-
 
 
 
 const Wrapper = styled.div`
     margin: 0px;
     padding: 0px;
-    min-height: 100% auto;
+    height: auto;
     background-color: #12112a;
-    display: flex;
+ 
     width:100%;
     text-align: center;
-    justify-content: center;
-    align-items: center;
+    background-color: #12112a;
 `
-
-const LoginContainer = styled.div`
-  height: 25rem;
-  width: 15rem;
-  background-color: white;
-  display: flex;
-  flex-direction: column;
-  border-radius: 2rem;
-`
-const LoginHeader = styled.h1`
-  padding-top: 2rem;
-  padding-bottom: 2rem;
-`
-const Textfield = styled(TextField)`
-  padding: 8px;
-`
-
 
 export default function Login() {
   return (
     <Wrapper>
-      <LoginContainer>
-        <LoginHeader>Login</LoginHeader>
-        <Box pt={0}>
-          <Textfield label="Email" placeholder="Enter Email" />
-        </Box>
 
-        <Box pt={2}>
-          <Textfield label="Password" placeholder="Enter Password" />
-        </Box>
-        <Box pt={2}>
-          <Button varient="outlined"> Log In</Button>
-
-        </Box>
-
-      
-      </LoginContainer>
     </Wrapper>
   )
 }
